34d8b09ab91fb3107b0ca2ffe857e18a31dd336d
Jacob Alexandre <49577844+JacobAraujo@users.noreply.github.com>
2023-01-20 22:23:21 -0300

    Update

--- buscas.py
+++ buscas.py
@@ -7,13 +7,45 @@ import matplotlib.pyplot as plt
 from IPython.display import display
 from collections import deque
 
-def adicionaEstadoFilho(arvore, estadoPai, direcao, nome):
+def getEstadoFilho(estadoPai, direcao, nome):
     jogoAdd = deepcopy(estadoPai[0]) 
     jogoAdd.move(direcao)
     estado = (jogoAdd, direcao, nome)
-    arvore.add_node(nome)
-    nx.set_node_attributes(arvore, estado, nome)
     return estado
+
+def adicionaNaArvore(arvore, estado):
+    arvore.add_node(estado[2])
+    nx.set_node_attributes(arvore, estado, estado[2])
+    
+def getFilhos(estado, direcoes):
+    filhos = []
+    cont = estado[2]
+    for direcao in direcoes:
+        filhos.append(getEstadoFilho(estado, direcao, cont))
+        cont += 1    
+    return filhos
+
+def ordenaFilhos(filhos):
+    distanciasDeManhattan = []
+    filhosOrdenados = []
+    for filho in filhos:
+        filhosOrdenados.append(filho)        
+    for filho in filhos:
+        distanciasDeManhattan.append(filho[0].distanciaDeManhattan())
+    distanciasDeManhattan.sort()
+    for filho in filhos:
+        lugarCerto = distanciasDeManhattan.index(filho[0].distanciaDeManhattan())
+        filhosOrdenados[lugarCerto] = filho
+    return filhosOrdenados
+
+def personalizaEscolha(filhos):
+    filhosPersonalizado = []
+    num = 40
+    for filho in filhos:
+        for i in range(num):
+            filhosPersonalizado.append(filho)
+        num -= 10
+    return filhosPersonalizado
     
 def buscaEmProfundidade():
     jogo = JogoDosOito()
@@ -39,15 +71,6 @@ def buscaEmProfundidade():
             estadoMelhor = estadoAtual[0]
             estadoMelhor.imprime()
             print(estadoMelhor.quantidadePosicoesErradas())
-        
-        # print('\n')
-        # for i in pilha:
-        #     print(i[2], end='')
-        # print('\n')
-    
-        # for i in pilha:
-        #     print(i[2], ': ')
-        #     i[0].imprime()
             
         if estadoAtual[0].estaCerto():
             print('esta certo')
@@ -59,7 +82,11 @@ def buscaEmProfundidade():
         direcoes = estadoAtual[0].movimentosPossiveis()
         random.shuffle(direcoes)
         for direcao in direcoes:
-            estadoFilho = adicionaEstadoFilho(arvoreDeBusca, estadoAtual, direcao, cont)
+            estadoFilho = getEstadoFilho(estadoAtual, direcao, cont)
+            for teste in visitados:
+                if estadoFilho[0].igual(teste):
+                    continue
+            adicionaNaArvore(arvoreDeBusca, estadoFilho)
             pilha.append(estadoFilho)
             arvoreDeBusca.add_edge(estadoAtual[2], cont)
             # nx.draw(arvoreDeBusca, with_labels=True, node_size=1200, node_color='red')
@@ -96,15 +123,13 @@ def buscaEmLargura():
                 break            
             direcoes = node[0].movimentosPossiveis()
             for direcao in direcoes:
-                estadoFilho = adicionaEstadoFilho(arvoreDeBusca, node, direcao, cont)
+                filho = getEstadoFilho(node, direcao, cont)
+                adicionaNaArvore(arvoreDeBusca, filho)
+                arvoreDeBusca.add_edge(node[2], filho[2])
                 cont += 1
-                fronteira[-1].append(estadoFilho)
-                arvoreDeBusca.add_edge(node[2], estadoFilho[2])
+                fronteira[-1].append(filho)
                 # nx.draw(arvoreDeBusca, with_labels=True, node_size=1200, node_color='red')
                 # plt.show()
         if flag:
             break
-                
-# buscaEmProfundidade()
-buscaEmLargura()
-                
+
