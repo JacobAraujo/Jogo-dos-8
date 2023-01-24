b6552c90517fba00af62ab97dccb97c3dfd60eb0
Emanuel Mendes Monteiro <emanuelmendes2001@gmail.com>
2023-01-23 21:28:45 -0300

    Manhattan (ainda não testado!)

--- buscas.py
+++ buscas.py
@@ -1,42 +1,50 @@
-from jogo import JogoDosOito
-import networkx as nx
-from copy import deepcopy
 import random
 from collections import deque
+from copy import deepcopy
+
+import networkx as nx
+
+from jogo import JogoDosOito
 
 # Falta oegar o caminho até a solução. Vou fazer isso usando o propriedade "direcao" na tupla "estado" para
 # retroceder o estado até o estado inicial pegando o caminho inverso
 
+
 def getEstadoFilho(estadoPai, direcao, nome, profundidade):
-    jogoAdd = deepcopy(estadoPai[0]) 
+    jogoAdd = deepcopy(estadoPai[0])
     jogoAdd.move(direcao)
     estado = (jogoAdd, direcao, nome, profundidade)
     return estado
 
+
 def adicionaNaArvore(arvore, estado):
     arvore.add_node(estado[2])
     nx.set_node_attributes(arvore, estado, estado[2])
-    
+
+
 def getFilhos(estado, direcoes):
     filhos = []
     cont = estado[2]
     for direcao in direcoes:
         filhos.append(getEstadoFilho(estado, direcao, cont))
-        cont += 1    
+        cont += 1
     return filhos
+
 
 def ordenaFilhos(filhos):
     distanciasDeManhattan = []
     filhosOrdenados = []
     for filho in filhos:
-        filhosOrdenados.append(filho)        
+        filhosOrdenados.append(filho)
     for filho in filhos:
         distanciasDeManhattan.append(filho[0].distanciaDeManhattan())
     distanciasDeManhattan.sort()
     for filho in filhos:
-        lugarCerto = distanciasDeManhattan.index(filho[0].distanciaDeManhattan())
+        lugarCerto = distanciasDeManhattan.index(
+            filho[0].distanciaDeManhattan())
         filhosOrdenados[lugarCerto] = filho
     return filhosOrdenados
+
 
 def personalizaEscolha(filhos):
     filhosPersonalizado = []
@@ -46,61 +54,65 @@ def personalizaEscolha(filhos):
             filhosPersonalizado.append(filho)
         num -= 10
     return filhosPersonalizado
-    
+
+
 def buscaEmProfundidade():
     jogo = JogoDosOito()
     jogo.jogo[0] = [1, 2, ' ']
     jogo.jogo[1] = [4, 5, 6]
     jogo.jogo[2] = [3, 7, 8]
 
-    estadoInicial = (jogo, '', 0, 0) # jogo, direcao de que veio, nome, profundidade
+    # jogo, direcao de que veio, nome, profundidade
+    estadoInicial = (jogo, '', 0, 0)
 
     cont = 1
     menor = 10
     pilha = [estadoInicial]
     visitados = []
     estadoMelhor = estadoInicial
-    
+
     profundidadeMaxima = 31
-    while pilha and cont <100000:
-        estadoAtual = pilha.pop() # pilha = [1, 3, 4]
+    while pilha and cont < 100000:
+        estadoAtual = pilha.pop()  # pilha = [1, 3, 4]
         # print('Estado atual: ')
-        # estadoAtual[0].imprime()  
-        
+        # estadoAtual[0].imprime()
+
         if estadoAtual[0].quantidadePosicoesErradas() <= menor:
             menor = estadoAtual[0].quantidadePosicoesErradas()
             estadoMelhor = estadoAtual[0]
             estadoMelhor.imprime()
             print(estadoMelhor.quantidadePosicoesErradas())
-            
+
         if estadoAtual[0].estaCerto():
             print('esta certo')
             break
         visitados.append(estadoAtual[0])
-        
+
         if estadoAtual[3] > profundidadeMaxima:
             continue
-        
+
         for teste in visitados:
             if estadoAtual[0].igual(teste):
                 continue
         direcoes = estadoAtual[0].movimentosPossiveis()
         random.shuffle(direcoes)
         for direcao in direcoes:
-            estadoFilho = getEstadoFilho(estadoAtual, direcao, cont, estadoAtual[3] + 1)
+            estadoFilho = getEstadoFilho(
+                estadoAtual, direcao, cont, estadoAtual[3] + 1)
             pilha.append(estadoFilho)
             cont += 1
     print(cont)
-    
+
+
 def buscaEmLargura():
     jogo = JogoDosOito()
     # jogo.jogo[0] = [1, ' ', 3]
     # jogo.jogo[1] = [2, 5, 6]
     # jogo.jogo[2] = [4, 7, 8]
     jogo.jogoAleatorio()
-    
-    estadoInicial = (jogo, '', 0) # jogo, direcao de que veio, nome
-    
+
+    estadoInicial = (jogo, '', 0)  # jogo, direcao de que veio, nome
+
     menor = 10
     cont = 1
     profundidade = 0
@@ -108,10 +120,10 @@ def buscaEmLargura():
     fronteira = []
     fronteira.append([estadoInicial])
     fronteira.append([])
-    
-    while(profundidade < 31):
+
+    while (profundidade < 31):
         while fronteira[0]:
-            node = fronteira[0].pop() 
+            node = fronteira[0].pop()
             if node[0].quantidadePosicoesErradas() <= menor:
                 menor = node[0].quantidadePosicoesErradas()
                 estadoMelhor = node[0]
@@ -120,7 +132,7 @@ def buscaEmLargura():
             if node[0].estaCerto():
                 print('esta certo')
                 flag = True
-                break            
+                break
             direcoes = node[0].movimentosPossiveis()
             for direcao in direcoes:
                 filho = getEstadoFilho(node, direcao, cont, profundidade+1)
@@ -134,5 +146,6 @@ def buscaEmLargura():
         print('Profundidade: ', profundidade)
         if flag:
             break
+
 
 buscaEmLargura()
--- jogo.py
+++ jogo.py
@@ -1,20 +1,63 @@
+import math
 import random
 
+
 class JogoDosOito:
-    
-    objetivo = [[1,2,3],[4,5,6],[7,8,' ']]
-    
+
+    objetivo = [[1, 2, 3], [4, 5, 6], [7, 8, ' ']]
+
     def __init__(self):
         self.jogo = []
         for i in range(3):
-            self.jogo.append([[],[],[]])
+            self.jogo.append([[], [], []])
 
     # carrega uma tabela aleatória do jogo dos 8
     def jogoAleatorio(self):
         valores = [1, 2, 3, 4, 5, 6, 7, 8, ' ']
         for i in range(3):
             for j in range(3):
-                self.jogo[i][j] = valores.pop(random.randint(0, len(valores)-1))
+                self.jogo[i][j] = valores.pop(
+                    random.randint(0, len(valores)-1))
+
+    def distanciaManhattan(self, manhattan):
+        sumo = 0
+
+        for i in range(3):
+            for j in range(3):
+                sumo += manhattan(self.jogo[i][j], i, j)
+
+        return sumo
+
+    # Distância manhattan de fato
+    def manhattan(p, i, j):
+        val1 = 0
+        val2 = 0
+        if p == 1:
+            val1 = i
+            val2 = j
+        elif p == 2:
+            val1 = i
+            val2 = j - 1
+        elif p == 3:
+            val1 = i
+            val2 = j - 2
+        elif p == 4:
+            val1 = i - 1
+            val2 = j
+        elif p == 5:
+            val1 = i - 1
+            val2 = j - 1
+        elif p == 6:
+            val1 = i - 1
+            val2 = j - 2
+        elif p == 7:
+            val1 = i - 2
+            val2 = j
+        else:
+            val1 = i - 2
+            val2 = j - 1
+
+        return math.abs(val1) + math.abs(val2)
 
     # imprime no formato de tabela -> pode ser substituido pela interface
     def imprime(self):
@@ -26,8 +69,8 @@ class JogoDosOito:
     def estaCerto(self):
         return self.jogo == JogoDosOito.objetivo
 
-
     # retorna um vetor[i, j] que vai indicar a posicao de num
+
     def posicao(self, num):
         for i in range(3):
             for j in range(3):
@@ -39,24 +82,28 @@ class JogoDosOito:
         if direcao == 'esquerda':
             posicaoDoVazio = self.posicao(' ')
             posicaoDoNumero = [posicaoDoVazio[0], posicaoDoVazio[1]-1]
-            self.jogo[posicaoDoVazio[0]][posicaoDoVazio[1]] = self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]]
+            self.jogo[posicaoDoVazio[0]][posicaoDoVazio[1]
+                                         ] = self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]]
             self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]] = ' '
         elif direcao == 'direita':
             posicaoDoVazio = self.posicao(' ')
             posicaoDoNumero = [posicaoDoVazio[0], posicaoDoVazio[1]+1]
-            self.jogo[posicaoDoVazio[0]][posicaoDoVazio[1]] = self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]]
+            self.jogo[posicaoDoVazio[0]][posicaoDoVazio[1]
+                                         ] = self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]]
             self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]] = ' '
         elif direcao == 'cima':
             posicaoDoVazio = self.posicao(' ')
             posicaoDoNumero = [posicaoDoVazio[0]-1, posicaoDoVazio[1]]
-            self.jogo[posicaoDoVazio[0]][posicaoDoVazio[1]] = self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]]
+            self.jogo[posicaoDoVazio[0]][posicaoDoVazio[1]
+                                         ] = self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]]
             self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]] = ' '
         elif direcao == 'baixo':
             posicaoDoVazio = self.posicao(' ')
             posicaoDoNumero = [posicaoDoVazio[0]+1, posicaoDoVazio[1]]
-            self.jogo[posicaoDoVazio[0]][posicaoDoVazio[1]] = self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]]
+            self.jogo[posicaoDoVazio[0]][posicaoDoVazio[1]
+                                         ] = self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]]
             self.jogo[posicaoDoNumero[0]][posicaoDoNumero[1]] = ' '
-            
+
     def movimentosPossiveis(self):
         # verifica quais movimentos podem ser feitos e retorna as direçoes
         direcoes = ['esquerda', 'direita', 'baixo', 'cima']
@@ -70,34 +117,36 @@ class JogoDosOito:
         if posicaoDoVazio[1] == 2:
             direcoes.remove('direita')
         return direcoes
-    
+
     def igual(self, jogoComparado):
         for i in range(3):
             for j in range(3):
                 if self.jogo[i][j] != jogoComparado.jogo[i][j]:
                     return False
         return True
-    
+
     def quantidadePosicoesErradas(self):
         cont = 0
         for i in range(3):
             for j in range(3):
                 if self.jogo[i][j] != JogoDosOito.objetivo[i][j]:
                     cont += 1
-        return cont 
-    
+        return cont
+
     def distanciaDeManhattan(self):
         cont = 0
         num = 1
         posicaoAtual = [0, 0]
         for i in range(3):
             for j in range(3):
-               posicaoAtual = self.posicao(num)
-               distanciaHorizontal = abs(i - posicaoAtual[0])
-               distanciaVertical = abs(j - posicaoAtual[1])
-               cont += distanciaHorizontal + distanciaVertical
-               if type(num) == int: num += 1
-               if num == 9: num = ' '
+                posicaoAtual = self.posicao(num)
+                distanciaHorizontal = abs(i - posicaoAtual[0])
+                distanciaVertical = abs(j - posicaoAtual[1])
+                cont += distanciaHorizontal + distanciaVertical
+                if type(num) == int:
+                    num += 1
+                if num == 9:
+                    num = ' '
         return cont
 
 # jogo = JogoDosOito()
@@ -105,7 +154,3 @@ class JogoDosOito:
 # jogo.imprime()
 
 # print(jogo.distanciaDeManhattan())
-        
-    
-
-    
